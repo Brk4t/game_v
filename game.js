85f793ce042c14cf3876ba03c938d704c1eb9a06
Brk4t <30989055+Brk4t@users.noreply.github.com>
2017-08-15 14:10:40 +0200

    all colors via img

--- game.js
+++ game.js
@@ -3,8 +3,31 @@
 var affichage;
 var img;
 function preload() {
-  tile_2 = loadImage("img/tile_2.png");
-  tile_2_over= loadImage("img/tile_2_over.png");
+  tile = Array(12);
+  tile[2] = loadImage("img/tile_2.png");
+  tile[3] = loadImage("img/tile_3.png");
+  tile[4] = loadImage("img/tile_4.png");
+  tile[5] = loadImage("img/tile_5.png");
+  tile[6] = loadImage("img/tile_6.png");
+  tile[7] = loadImage("img/tile_7.png");
+  tile[8] = loadImage("img/tile_8.png");
+  tile[9] = loadImage("img/tile_9.png");
+  tile[10] = loadImage("img/tile_10.png");
+  tile[11] = loadImage("img/tile_11.png");
+  
+  tile_over = Array(12);
+  tile_over[2] = loadImage("img/tile_2_over.png");
+  tile_over[3] = loadImage("img/tile_3_over.png");
+  tile_over[4] = loadImage("img/tile_4_over.png");
+  tile_over[5] = loadImage("img/tile_5_over.png");
+  tile_over[6] = loadImage("img/tile_6_over.png");
+  tile_over[7] = loadImage("img/tile_7_over.png");
+  tile_over[8] = loadImage("img/tile_8_over.png");
+  tile_over[9] = loadImage("img/tile_9_over.png");
+  tile_over[10] = loadImage("img/tile_10_over.png");
+  tile_over[11] = loadImage("img/tile_11_over.png");
+
+  
 }
 
 function setup() {
@@ -361,10 +384,9 @@ function brique(r, n) {
 
   this.display_over = function() 
   {
-        if (this.value == 2)
-    {
-      image(tile_2_over, this.x, this.y, tile_2_over.width*this.size/tile_2_over.width, tile_2_over.height*this.size/tile_2_over.height)
-    } else {
+    tile_size = tile_over[2].width;
+    image(tile_over[this.value], this.x, this.y, tile_size*this.size/tile_size, tile_size*this.size/tile_size)
+/* sans images :
     fill(this.col[this.value-2], 100);
     strokeWeight(4);
     stroke(0);
@@ -373,24 +395,25 @@ function brique(r, n) {
     textSize(80*7/this.n);
     strokeWeight(1);
     text(this.value, this.x+ this.size/2, this.y + 3*this.size/4);
-    }
+    */
   }
 
 
   this.display = function() 
   {
+    
+    tile_size = tile[2].width;
+    image(tile[this.value], this.x, this.y, tile_size*this.size/tile_size, tile_size*this.size/tile_size)
+    
+    /* sans image
     stroke(255);
-    if (this.value == 2)
-    {
-      image(tile_2, this.x, this.y, tile_2.width*this.size/tile_2.width, tile_2.height*this.size/tile_2.height)
-    } else {
       fill(this.col[this.value-2]);
       rect(this.x, this.y, this.size, this.size);
       fill(0, 0, 0);
       stroke(0);
       textSize(80*7/this.n);
       text(this.value, this.x+ this.size/2, this.y+3*this.size/4);
-    }
+    } */
   }
 
 
